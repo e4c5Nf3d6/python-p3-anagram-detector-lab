c0acae4453afa0cea19fb6e88b01924f35c603a0
Maria Jenkins <macjenkin@gmail.com>
2023-10-03 15:22:12 -0500

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, l):
+        matches = []
+        for x in l:
+            if sorted([*x.lower()]) == sorted([*self.word.lower()]):
+                matches.append(x)
+        return matches
